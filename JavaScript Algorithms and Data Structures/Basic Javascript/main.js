9e4f9bf94f9c051b10bc39ba8488a02e686a3d86
Justinshakes <Superjustin44@gmail.com>
2023-12-21 13:31:15 -0800

    Update main.js

--- JavaScript Algorithms and Data Structures/Basic Javascript/main.js
+++ JavaScript Algorithms and Data Structures/Basic Javascript/main.js
@@ -1,6 +1,8 @@
-var ourName
+const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+const conversation = 'Finn exclaims to Jake, "Algebraic!"';
+const goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
+// const badStr = 'Finn responds, "Let's go!"';
 
-// THis is it
-// Definitely not
-// Got it
-// sure you do
+console.log(myStr);
+console.log(conversation);
+console.log(goodStr);
