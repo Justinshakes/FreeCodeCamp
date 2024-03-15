f986dcd1742bf62dcc6191cf9929e0be7ad5135b
Justin Sherrell <Superjustin44@gmail.com>
2024-03-15 13:57:43 -0700

    Update script.js

--- JavaScript Algorithms and Data Structures/Section1/Calorie Counter/Redo/script.js
+++ JavaScript Algorithms and Data Structures/Section1/Calorie Counter/Redo/script.js
@@ -98,6 +98,4 @@ function clearForm() {
 
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
-addEntryButton.addEventListener("click", addEntry);
-calorieCounter.addEventListener("submit", calculateCalories);
 clearButton.addEventListener("click", clearForm);
