aa5daa3472d46c24588509e47bc8155ecb7fac8f
Justinshakes <Superjustin44@gmail.com>
2024-04-02 15:18:41 -0700

    step 47 cal counter

--- JavaScript Algorithms and Data Structures/Section1/Calorie Counter/Redo/script.js
+++ JavaScript Algorithms and Data Structures/Section1/Calorie Counter/Redo/script.js
@@ -16,4 +16,11 @@ function isInvalidInput(str) {
     return str.match(regex);
 }
 
-// Step 36
+// Step 47
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
+        }
