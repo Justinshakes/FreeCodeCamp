bbe063443ad7d113ec8c015899eae6040b660bea
Justin Sherrell <Superjustin44@gmail.com>
2024-10-02 19:06:28 -0700

    Finished Pyramid Generator

--- JavaScript Algorithms and Data Structures/Section1/1_Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures/Section1/1_Pyramid Generator/script.js
@@ -1,21 +1,18 @@
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
-
-let done = 0;
-
-while (done <= count) {
-    done++;
-    rows.push(padRow(done, count));
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+        rows.unshift(padRow(i, count));
+    } else {
+        rows.push(padRow(i, count));
+    }
 }
 
 let result = ""
@@ -25,5 +22,3 @@ for (const row of rows) {
 }
 
 console.log(result);
-
-// Step 95
