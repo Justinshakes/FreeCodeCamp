72cf674309a7bee388d9071723a2f152c8602436
Justin Sherrell <Superjustin44@gmail.com>
2024-10-03 20:28:14 -0700

    Update redoScript.js

--- JavaScript Algorithms and Data Structures/Section1/1_Pyramid Generator/redoScript.js
+++ JavaScript Algorithms and Data Structures/Section1/1_Pyramid Generator/redoScript.js
@@ -2,3 +2,26 @@
 const count = 10;
 const rows = [];
 let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+    console.log("rowNumber: " + rowNumber + "  rowCount: " + rowCount)
+    console.log(character.repeat(rowCount - rowNumber));
+    return character.repeat(rowCount - rowNumber) + " rowNumber: " + rowNumber;
+}
+
+for(let i = 1; i <= count; i++) {
+
+    console.log("i: " + i)
+    rows.unshift(padRow(i, count));
+}
+
+let result = "";
+
+for(const row of rows) {
+    result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+console.log(rows);
