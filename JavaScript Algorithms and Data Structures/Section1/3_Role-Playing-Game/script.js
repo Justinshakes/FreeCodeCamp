4c7b51f43a828badbf3184a825b8ad59a25607d9
Justin Sherrell <Superjustin44@gmail.com>
2024-10-06 03:08:01 -0700

    Continued Work on Roleplaying Game

---
+++ JavaScript Algorithms and Data Structures/Section1/3_Role-Playing-Game/script.js
@@ -0,0 +1,17 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#text");
+const healthText = document.querySelector("#text");
+const goldText = document.querySelector("#text");
+const text = document.querySelector("#text");
+const text = document.querySelector("#text");
